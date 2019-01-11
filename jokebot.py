66fb6c21cadfa64f8910db90def4253b3d4f5c2f
Kevin Tang <kevintang@MacBook-Pro-224.local>
2019-01-11 03:25:55 -0800

    final

--- jokebot.py
+++ jokebot.py
@@ -4,6 +4,14 @@ import sys
 import time
 import requests
 
+###########
+##Jokebot##
+###########
+# Input: a csv file with prompt and punchline columns or if one is not provided, retrieve appropriate jokes from r/dadjokes
+# Output: joke prompt, followed quickly by the punchline
+# the user will enter 'next' for more jokes or 'quit' to exit.
+
+
 def deliver_joke(prompt, punchline):
 # prints the prompt, waits two seconds, then prints the punchline.
     print(prompt)
@@ -17,7 +25,7 @@ def get_user_input():
     choice = input()
     if (choice != "next") and (choice != "quit"):
         print("Not a valid input. Input 'next' for another joke, or 'quit' to stop.")
-        choice = get_user_input()
+        return get_user_input()
     elif choice == "next":
         return True
     else:
@@ -28,23 +36,27 @@ def read_jokes(file):
     joke_list = []
     with open(file, newline='') as jokes:
         jokereader = csv.reader(jokes, delimiter=',')
-        for row in jokereader:
-            try:
-                joke_list.append([row[0], row[1]])
-            # in case the csv passed in has 0 or 1 columns    
-            except IndexError:
-                print('Joke list does not provide both a prompt and a punchline!')
+        for n, row in enumerate(jokereader):
+            # catch if a cell is empty when reading csv
+            if row[0] == '':
+                raise IndexError("No prompt on row %s" % n)
+            elif row[1] == '':
+                raise IndexError("No punchline on row %s" % n)
+
+            joke_list.append([row[0], row[1]])
+
     return joke_list       
 
 def get_jokes():
 # gets jokes from r/dadjokes by parsing a JSON, if a joke csv is not given. Jokes are only chosen if
-# the title begins with 'Why', 'How', or 'What', and are sfw.
+# the title begins with 'Why', 'How', or 'What', and are sfw
 
     # headers arg gets rid of too many requests error
     r = requests.get('https://www.reddit.com/r/dadjokes.json', headers = {'User-agent': 'your bot 0.1'})
     joke_list = []
     query_words = ['Why', 'How', 'What']
 
+    # iterate through all posts to get necessary data
     for post in r.json()['data']['children']:
         post_data = post['data']
         if (post_data['over_18'] == True) or (post_data['selftext'] == "") or (post_data['title'].partition(' ')[0] not in query_words):
@@ -54,7 +66,7 @@ def get_jokes():
 
     return joke_list
 
-# gets the csv file from the command line, else it defaults to getting from reddit.
+# gets the csv file from the command line, else it defaults to getting from reddit
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", '--filename', type=str, default = 'reddit', help = 'No joke file given.')
 args = parser.parse_args()
@@ -64,15 +76,15 @@ if args.filename == 'reddit':
 else:
     jokes = read_jokes(args.filename)
 
-# In the case no jokes are in jokelist from either empty csv or no jokes from reddit meet our conditions.
+# In the case no jokes are in jokelist from either empty csv or no jokes from reddit meet our conditions
 if jokes == None:
-    if args.filename == 'reddit'
+    if args.filename == 'reddit':
         print('No jokes from reddit meet criteria!')
     else: 
         print('Joke list is empty!')
     sys.exit(0)
 
-# iterate over all jokes and continue based on user input, if out of jokes, then stop.
+# iterate over all jokes and continue based on user input, if out of jokes, then stop
 for i , joke in enumerate(jokes):
     deliver_joke(joke[0],joke[1])
     option = get_user_input()
